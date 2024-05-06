a40e56859d9ba1ce202387b49891eea042b930a7
Okan <co.okancelik@gmail.com>
2024-05-06 15:03:33 +0200

    added employee and model

--- src/main/java/com/okan/contoroller/EmployeeController.java
+++ src/main/java/com/okan/contoroller/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.okan.contoroller;
 
+import com.okan.bootstrap.DataGenerator;
+import com.okan.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
     @GetMapping("employee")
-    public String createEmployee(){
+    public String createEmployee(Model model){
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
+
         return "employee/employee-create";
+    }
+    @PostMapping("/insert")
+    public String insertEmployee(){
+
     }
 }
